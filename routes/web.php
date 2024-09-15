ae8a6422a6350a89657d2112916f8e605e388a03
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-09-16 01:19:54 +0900

    サクラサーバーように、web.php内のトップページのrouting修正

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\MapController;
 
-Route::get('/', function () {
+Route::get('/sdm', function () {
     return view('welcome');
 });
 
