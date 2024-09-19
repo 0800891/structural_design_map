d048f4487db32f4ac0ce12870d8ee5d1000541b5
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-09-20 00:34:36 +0900

    プロジェクト詳細に画像追加

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,17 +8,26 @@
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-          <div class="p-6 text-gray-900 dark:text-gray-100">
+          <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-wrap" style="max-width: 100%;">
             @foreach ($projects as $project)
-            <div class="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
+            <div class="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg basis-1/4">
               <p class="text-gray-800 dark:text-gray-300">{{ $project->project }}</p>
-              <p class="text-gray-600 dark:text-gray-400 text-sm"> <a href="{{ route('projects.show', $project) }}" class="text-blue-500 hover:text-blue-700">{{$project->name}} </a>/ Structural Designer: {{ $project->company->name }} </p>
+              <p class="text-gray-600 dark:text-gray-400 text-sm"> 
+                <a href="{{ route('projects.show', $project) }}" class="text-blue-500 hover:text-blue-700">{{$project->name}} </a>/ Structural Designer: {{ $project->company->name }}
+              </p>
+                @if($project->picture_01_link)
+                  {{-- <a href="{{ asset($project->picture_01_link) }}" data-lightbox="group"> --}}
+                    <img src="{{ asset($project->picture_01_link) }}" alt="Picture 01" class="w-32 h-32 object-cover">
+                {{-- </a> --}}
+                @endif
+                
             </div>
             @endforeach
           </div>
         </div>
       </div>
     </div>
+
   
   </x-app-layout>
   
