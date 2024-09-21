618c2ed80c1060715c5b97fd3e9ce8a8490c5f32
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-09-21 23:30:09 +0900

    dashboard 修正

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,14 +25,15 @@
 
                     {{-- Display liked projects --}}
                     <h3 class="text-lg font-semibold mt-6 px-6">{{ __('Liked Projects') }}</h3>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 px-6">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 px-6 flex flex-wrop">
                         @foreach ($likedProjects as $project)
-                        <div class="border mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
+                        <div class="border mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg flex-grow">
                             {{-- <p class="text-gray-800 dark:text-gray-300">{{ $project->name }}</p> --}}
                             <p class="text-gray-600 dark:text-gray-400 text-sm">
                                 <a href="{{ route('projects.show', $project) }}" class="text-blue-500 hover:text-blue-700">{{ $project->name }}</a>
                             </p>
                             <p class="text-gray-600 dark:text-gray-400 text-sm">{{ $project->company->name }}</p>
+                            
                             @if($project->picture_01_link)
                   {{-- <a href="{{ asset($project->picture_01_link) }}" data-lightbox="group"> --}}
                     <img src="{{ asset($project->picture_01_link) }}" alt="Picture 01" class="w-32 h-32 object-cover">
