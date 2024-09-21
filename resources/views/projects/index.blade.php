bffb925992b5a6b98e5995416cf8778a66c8ecb5
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-09-21 17:17:16 +0900

    再修正

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -13,8 +13,9 @@
             <div class="border mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg flex-grow">
               <p class="text-gray-800 dark:text-gray-300">{{ $project->project }}</p>
               <p class="text-gray-600 dark:text-gray-400 text-sm"> 
-                <a href="{{ route('projects.show', $project) }}" class="text-blue-500 hover:text-blue-700">{{$project->name}} </a>/ {{ $project->company->name }}
+                <a href="{{ route('projects.show', $project) }}" class="text-blue-500 hover:text-blue-700">{{$project->name}} </a>
               </p>
+              <p class="text-gray-600 dark:text-gray-400 text-sm">/ {{ $project->company->name }}</p>
                 @if($project->picture_01_link)
                   {{-- <a href="{{ asset($project->picture_01_link) }}" data-lightbox="group"> --}}
                     <img src="{{ asset($project->picture_01_link) }}" alt="Picture 01" class="w-32 h-32 object-cover">
