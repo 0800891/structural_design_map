9118a43888f805a36a4ff292fbb6b70a0536e545
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-09-28 23:01:33 +0900

    Translation Resultのページの更新

--- resources/views/translation.blade.php
+++ resources/views/translation.blade.php
@@ -1,18 +1,32 @@
 <!-- resources/views/translation.blade.php -->
-
+<x-app-layout>
+    <x-slot name="header" style="background-color:rgb(236,230,198)">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Translation Result') }}
+        </h2>
+    </x-slot>
 <html>
 <head>
     <meta charset='utf-8' />
+     <!-- Favicon -->
+     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
 </head>
 <body>
-    <h2>Translation Result</h2>
-    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
     <p hidden><strong>Original Text:</strong></p>
     <p hidden>{{ $sentence }}</p>
 
-    <p><strong>Translated to {{ $target_lang }}:</strong></p>
-    <p>{{ $translated_text }}</p>
+    <p class="p-4"><strong>Translated to {{ $target_lang }}:</strong></p>
+    <p class="p-4">{{ $translated_text }}</p>
 
-    <a href="{{ url()->previous() }}">Go Back</a>
+    <a href="{{ url()->previous() }}" class="text-blue-500 p-4">Go Back</a>
+          </div>
+        </div>
+    </div>
+
 </body>
 </html>
+
+</x-app-layout>
